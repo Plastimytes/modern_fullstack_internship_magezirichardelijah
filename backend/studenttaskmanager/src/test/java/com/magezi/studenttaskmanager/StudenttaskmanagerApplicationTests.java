d1c05d671d94430816a4baf9c4762f3a5f9e2c34
Magezi Richard Elijah <magezirichardelijah@gmail.com>
2026-06-17 16:14:40 +0300

    Backend OpenAPI and Swagger UI

---
+++ backend/studenttaskmanager/src/test/java/com/magezi/studenttaskmanager/StudenttaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magezi.studenttaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudenttaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
